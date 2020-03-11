078f23f6e299fdf9634c111805e4e22fed8ca3b5
Simin Lei <siminlei@simins-mbp.lan>
2020-03-10 20:35:39 -0700

    Added second seeding script for batching

--- seeds/runSeed.js
+++ seeds/runSeed.js
@@ -1,4 +1,10 @@
 const { DATABASE } = require("../tempConfig.js"); //NEED TO CHANGE
 const { save } = require(`./save-${ DATABASE }`);
+const { save2 } = require(`./save-${ DATABASE }2`);
+
 
 save();
+
+module.exports.save2 = function () {
+  save2();
+};
